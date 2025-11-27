5abc7026ccecd23dd4a1614b0672e7d1f8c1393f
adityadewhy-local <adityadewhy@gmail.com>
2025-11-27 12:34:18 +0530

    share page redirect added, add loader next

--- src/components/share.tsx
+++ src/components/share.tsx
@@ -1,9 +1,19 @@
+"use client"
+
 import {Share2} from "lucide-react";
+import {useRouter} from "next/navigation";
 
 export default function Share() {
+	const router = useRouter();
 	return (
-		<button className="justify-center rounded-md p-1.5 hover:bg-gray-700 text-gray-200 transition-colors " title="Share">
+		<button
+			onClick={() => router.push("/share")}
+			className="justify-center rounded-md p-1.5 hover:bg-gray-700 text-gray-200 transition-colors "
+			title="Share"
+		>
 			<Share2 />
 		</button>
 	);
 }
+
+// add loader
