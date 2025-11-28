4d39584996d3b476991ea7ca2e733ef066f6d344
adityadewhy-local <adityadewhy@gmail.com>
2025-11-28 08:48:34 +0530

    share-btn loader added

---
+++ src/components/ringLoader.tsx
@@ -0,0 +1,6 @@
+import {Tailspin} from "ldrs/react";
+import "ldrs/react/Tailspin.css";
+
+export default function RingLoader() {
+	return <Tailspin size="20" stroke="5" speed="0.9" color="white" />;
+}
--- src/components/share.tsx
+++ src/components/share.tsx
@@ -1,17 +1,32 @@
-"use client"
+"use client";
 
 import {Share2} from "lucide-react";
 import {useRouter} from "next/navigation";
+import {useState} from "react";
+import RingLoader from "./ringLoader";
 
 export default function Share() {
 	const router = useRouter();
+	const [loading, setLoading] = useState(false);
+
+	const handleClick = () => {
+		setLoading(true);
+		router.push("/share");
+	};
+
+	let shareORloader;
+	if (loading) {
+		shareORloader = <RingLoader />;
+	} else {
+		shareORloader = <Share2 />;
+	}
 	return (
 		<button
-			onClick={() => router.push("/share")}
-			className="justify-center rounded-md p-1.5 hover:bg-gray-700 text-gray-200 transition-colors "
+			onClick={handleClick}
+			className="justify-center items-center rounded-md p-1.5 hover:bg-gray-700 text-gray-200 transition-colors "
 			title="Share"
 		>
-			<Share2 />
+			{shareORloader}
 		</button>
 	);
 }
